43841990092b293d2876a5a5a96e2c140118fc82
Harry Han <yqszxx@gmail.com>
2016-10-09 00:00:10 +0800

    安全起见，不置空cookie_domain
    按照之前的配置，如果主域名设为ip地址就将cookie_domain，安全系数降低（不排除网站在公网裸奔或有人在内网搭建窃取sessionid的服务器），故改之。修改后仍然可以适配泛域名。

--- uoj/1/app/models/Session.php
+++ uoj/1/app/models/Session.php
@@ -3,11 +3,9 @@
 class Session {
 	public static function init() {
 		$domain = UOJConfig::$data['web']['domain'];
-		if (validateIP($domain)) {
-			$domain = '';
-		} else {
-			$domain = '.'.$domain;
-		}
+                if (!validateIP($domain)) {
+                        $domain = '.'.$domain;
+                }
 		
 		session_name('UOJSESSID');
 		ini_set('session.cookie_path', '/');
